60c873d6f8226efd2f4ed9cb84daf1bae87acaee
Marko Baskarad <marko.baskarad34@gmail.com>
2021-01-14 21:33:23 +0100

    add component TodoApp and set to render in App.js , and also change the title of an application

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class TodoApp extends Component {
+  render() {
+    return (
+      <div className="todo-app">
+        <h1>My First Todo App</h1>
+      </div>
+    );
+  }
+}
+
+export default TodoApp;
